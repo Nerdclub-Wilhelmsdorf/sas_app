7cfbdf391d55a1d2b09b19bf190ca86a8efe51bf
Len <mail@lenblum.de>
2024-12-17 20:23:30 +0100

    fixing dropdown

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,7 @@
     import { Navbar, NavBrand, NavLi, NavUl, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
     import { Menu} from "lucide-svelte";
     import { logout } from '../src/login.svelte';
+    import { CurrentPage, currentPage } from '../src/stores.svelte';
 
     let dropdownOpen = $state(false);
 
@@ -19,11 +20,14 @@
   <NavBrand href="/">
       <span class="text-xl font-semibold dark:text-white">SaS Pay</span>
   </NavBrand>
-  <Menu class="no-outline outline-none" on:click={handleDropdownClick} />
+  <Menu class="no-outline outline-none" onclick={handleDropdownClick} />
       <Dropdown open={dropdownOpen}>
+        {#if currentPage.page != CurrentPage.Login}
           <DropdownItem
               on:click={() => handleItemClick(logout)}
           >Konto zurücksetzen</DropdownItem>
+          {/if}
           <DropdownItem>Version 0.1</DropdownItem>
+
       </Dropdown>
 </Navbar>
